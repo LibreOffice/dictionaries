130354d4fc0669036d0256f840a13e2d8907161b
Kevin Hendricks <khendricks@openoffice.org>
2003-06-05 15:45:02 +0000

    fixing HEAD to match cws_srx644_ooo11beta2

--- dictionaries/util/makefile.mk
+++ dictionaries/util/makefile.mk
@@ -1,11 +1,11 @@
 PRJ=..
 PRJNAME=dictionaries
-TARGET=writingaids
+TARGET=pack_writing_aids
 
 .INCLUDE : settings.mk
 
 ZIP1TARGET      = writingaids
-ZIP1LIST        = $(BIN)$/*.dic $(BIN)$/*.aff $(BIN)$/*.txt $(BIN)$/dictionary.lst $(BIN)$/*.idx $(BIN)$/*.dat
+ZIP1LIST        = $(BIN)$/*.dic $(BIN)$/*.aff $(BIN)$/*.txt $(BIN)$/*.lst $(BIN)$/*.idx $(BIN)$/*.dat
 
 .INCLUDE : target.mk
 
