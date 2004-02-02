f695b3eb4b49bd6d1f5fe9ecfdaa1d04ae32204c
Jens-Heiner Rechtien <hr@openoffice.org>
2004-02-02 19:54:14 +0000

    INTEGRATION: CWS geordi2q14 (1.2.34); FILE MERGED 2004/01/29 15:28:10 hr 1.2.34.1: #111934#: merge CWS ooo111fix2

--- dictionaries/util/makefile.mk
+++ dictionaries/util/makefile.mk
@@ -5,7 +5,7 @@ TARGET=pack_writing_aids
 .INCLUDE : settings.mk
 
 ZIP1TARGET      = writingaids
-ZIP1LIST        = $(BIN)$/*.dic $(BIN)$/*.aff $(BIN)$/*.txt $(BIN)$/*.lst $(BIN)$/*.idx $(BIN)$/*.dat
+ZIP1LIST        = $(BIN)$/*.dic $(BIN)$/*.aff $(BIN)$/*.txt $(BIN)$/*.lst $(BIN)$/*.idx $(BIN)$/*.dat $(BIN)$/*.sxw
 
 .INCLUDE : target.mk
 
