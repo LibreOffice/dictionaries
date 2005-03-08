74ecbae701d013fd480151f04c30305f303c9de3
Vladimir Glazounov <vg@openoffice.org>
2005-03-08 15:23:29 +0000

    INTEGRATION: CWS ause023 (1.3.36); FILE MERGED 2005/02/28 15:20:06 hjs 1.3.36.1: #i43541# applied complete mp patch; output handling of th_gen_idx.pl

--- dictionaries/util/makefile.mk
+++ dictionaries/util/makefile.mk
@@ -4,8 +4,20 @@ TARGET=pack_writing_aids
 
 .INCLUDE : settings.mk
 
+DICDEPS:=$(subst,/,$/ $(shell $(FIND) $(subst,$/,/ $(BIN)) -name "*.line"))
+
+
 ZIP1TARGET      = writingaids
 ZIP1LIST        = $(BIN)$/*.dic $(BIN)$/*.aff $(BIN)$/*.txt $(BIN)$/*.lst $(BIN)$/*.idx $(BIN)$/*.dat $(BIN)$/*.sxw
 
 .INCLUDE : target.mk
+
+.IF "$(ZIP1TARGETN)" != ""
+$(ZIP1TARGETN) : $(BIN)$/dictionary.lst
+.ENDIF			# "$(ZIP1TARGETN)" != ""
+
+$(BIN)$/dictionary.lst : $(DICDEPS)
+    @+-$(RM) $@
+    +$(TYPE) $(BIN)$/dictionary_head > $@
+    +-$(TYPE) $(BIN)$/dictionary_*.line >> $@
 
