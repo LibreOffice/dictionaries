d1efe5b98aa9c2f18c85b9163b1b680287999951
Kurt Zenker <kz@openoffice.org>
2006-04-27 08:41:26 +0000

    INTEGRATION: CWS os80 (1.2.6); FILE MERGED 2006/04/13 10:49:37 tl 1.2.6.2: #i63324# include German thesaurus 2006/04/12 12:40:11 tl 1.2.6.1: #i63324# include German thesaurus

--- dictionaries/de_CH/makefile.mk
+++ dictionaries/de_CH/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: rt $ $Date: 2006-02-10 10:24:23 $
+#   last change: $Author: kz $ $Date: 2006-04-27 09:41:26 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -62,4 +62,16 @@ DIC2BIN= \
 
 .INCLUDE : target.mk
 .INCLUDE : $(PRJ)$/util$/target.pmk
+
+#
+# add thesaurus for de_CH
+#
+.IF "$(DIC_ALL)$(DIC_DECH)"!=""
+
+ALLTAR : $(MISC)$/thes_de_CH_v2.don
+
+$(MISC)$/thes_de_CH_v2.don: thes_de_CH_v2.zip
+    +cd $(BIN) && $(WRAPCMD) unzip -o $(PWD)$/thes_de_CH_v2.zip
+
+.ENDIF
 
