002a9e168fbe6e57cba8e0b8cd8c2df370c6ce83
Jens-Heiner Rechtien <hr@openoffice.org>
2007-01-02 14:03:06 +0000

    INTEGRATION: CWS oolinguforso (1.1.2); FILE ADDED 2006/10/25 07:30:58 tl 1.1.2.3: #142482# makefile fixed to provide missing entry for writingaids.zip's dictionary.lst file 2006/10/25 06:51:30 tl 1.1.2.2: #142482# makefile fixed to provide missing entry for writingaids.zip's dictionary.lst file 2006/10/23 09:43:02 tl 1.1.2.1: #142482# integration of Polish thesaurus

---
+++ dictionaries/pl_PL/makefile.mk
@@ -0,0 +1,67 @@
+#*************************************************************************
+#
+#   $RCSfile: makefile.mk,v $
+#
+#   $Revision: 1.2 $
+#
+#   last change: $Author: hr $ $Date: 2007-01-02 15:03:06 $
+#
+#   The Contents of this file are made available subject to
+#   the terms of GNU Lesser General Public License Version 2.1.
+#
+#
+#     GNU Lesser General Public License Version 2.1
+#     =============================================
+#     Copyright 2005 by Sun Microsystems, Inc.
+#     901 San Antonio Road, Palo Alto, CA 94303, USA
+#
+#     This library is free software; you can redistribute it and/or
+#     modify it under the terms of the GNU Lesser General Public
+#     License version 2.1, as published by the Free Software Foundation.
+#
+#     This library is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#     Lesser General Public License for more details.
+#
+#     You should have received a copy of the GNU Lesser General Public
+#     License along with this library; if not, write to the Free Software
+#     Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#     MA  02111-1307  USA
+#
+#*************************************************************************
+
+PRJ = ..
+
+PRJNAME	= dictionaries
+TARGET  = dict_pl_PL
+
+#----- Settings ---------------------------------------------------------
+
+.INCLUDE : settings.mk
+
+# --- Files --------------------------------------------------------
+
+.IF "$(DIC_ALL)$(DIC_PLPL)"!="" 
+
+# DIC2BIN must be non-empty in order to generate the respective lines for
+# Polish in the dictionary.lst of the writingaids.zip
+DIC2BIN = th_pl_PL_v2.zip
+
+.ENDIF
+
+# --- Targets ------------------------------------------------------
+
+.INCLUDE : target.mk
+.INCLUDE : $(PRJ)$/util$/target.pmk
+
+.IF "$(DIC_ALL)$(DIC_PLPL)"!=""
+
+ALLTAR : $(MISC)$/th_pl_PL_v2.don
+
+$(MISC)$/th_pl_PL_v2.don: th_pl_PL_v2.zip
+    +cd $(BIN) && $(WRAPCMD) unzip -o $(PWD)$/th_pl_PL_v2.zip
+# .idx file is already part of the zip-archive
+#    +$(PERL) $(PRJ)$/util$/th_gen_idx.pl -o $(BIN)$/th_pl_PL_v2.idx <$(BIN)$/th_pl_PL_v2.dat && $(TOUCH) $(MISC)$/th_pl_PL_v2.don
+
+.ENDIF          # "$(DIC_ALL)$(DIC_PLPL)"!=""
