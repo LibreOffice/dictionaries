01cf206f69806aa7ed65f877403e8a15d6eeaa0a
Oliver Bolte <obo@openoffice.org>
2007-01-25 12:40:02 +0000

    INTEGRATION: CWS ause069 (1.7.44); FILE MERGED 2006/11/24 15:09:35 hjs 1.7.44.1: #i70359# remove forced shell calls

--- dictionaries/diclst/makefile.mk
+++ dictionaries/diclst/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.7 $
+#   $Revision: 1.8 $
 #
-#   last change: $Author: rt $ $Date: 2005-09-08 17:56:57 $
+#   last change: $Author: obo $ $Date: 2007-01-25 13:40:02 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -55,7 +55,7 @@ DIC2BIN+=	FontOOo.sxw
 .ENDIF
 
 $(MISC)$/$(TARGET).don : $(DIC2BIN)
-    +$(COPY) $(foreach,i,$(DIC2BIN) $i) $(BIN)
+    $(COPY) $(foreach,i,$(DIC2BIN) $i) $(BIN)
     @$(TOUCH) $@
 
 # --- Targets ------------------------------------------------------
