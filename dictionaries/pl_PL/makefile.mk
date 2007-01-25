6f60fed898c8c3ad04d3dabc6de7b898f3439d50
Oliver Bolte <obo@openoffice.org>
2007-01-25 12:40:29 +0000

    INTEGRATION: CWS ause069 (1.2.4); FILE MERGED 2007/01/17 14:30:18 hjs 1.2.4.1: #i70359# no forced shell required here

--- dictionaries/pl_PL/makefile.mk
+++ dictionaries/pl_PL/makefile.mk
@@ -2,9 +2,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.2 $
+#   $Revision: 1.3 $
 #
-#   last change: $Author: hr $ $Date: 2007-01-02 15:03:06 $
+#   last change: $Author: obo $ $Date: 2007-01-25 13:40:29 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -60,7 +60,7 @@ DIC2BIN = th_pl_PL_v2.zip
 ALLTAR : $(MISC)$/th_pl_PL_v2.don
 
 $(MISC)$/th_pl_PL_v2.don: th_pl_PL_v2.zip
-    +cd $(BIN) && $(WRAPCMD) unzip -o $(PWD)$/th_pl_PL_v2.zip
+    cd $(BIN) && $(WRAPCMD) unzip -o $(PWD)$/th_pl_PL_v2.zip
 # .idx file is already part of the zip-archive
 #    +$(PERL) $(PRJ)$/util$/th_gen_idx.pl -o $(BIN)$/th_pl_PL_v2.idx <$(BIN)$/th_pl_PL_v2.dat && $(TOUCH) $(MISC)$/th_pl_PL_v2.don
 
