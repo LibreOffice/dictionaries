8b89b3bc7463beddcd2ab26cf78f3ff80675c608
Jens-Heiner Rechtien <hr@openoffice.org>
2007-06-27 16:50:51 +0000

    INTEGRATION: CWS ause079 (1.4.4); FILE MERGED 2007/05/23 10:03:28 hjs 1.4.4.1: #i76902# fix dependencies

--- dictionaries/de_CH/makefile.mk
+++ dictionaries/de_CH/makefile.mk
@@ -4,9 +4,9 @@
 #
 #   $RCSfile: makefile.mk,v $
 #
-#   $Revision: 1.4 $
+#   $Revision: 1.5 $
 #
-#   last change: $Author: obo $ $Date: 2007-01-25 13:39:34 $
+#   last change: $Author: hr $ $Date: 2007-06-27 17:50:51 $
 #
 #   The Contents of this file are made available subject to
 #   the terms of GNU Lesser General Public License Version 2.1.
@@ -71,7 +71,7 @@ DIC2BIN= \
 ALLTAR : $(MISC)$/thes_de_CH_v2.don
 
 $(MISC)$/thes_de_CH_v2.don: thes_de_CH_v2.zip
-    cd $(BIN) && $(WRAPCMD) unzip -o $(PWD)$/thes_de_CH_v2.zip
+    cd $(BIN) && $(WRAPCMD) unzip -o $(PWD)$/thes_de_CH_v2.zip && $(TOUCH) ..$/misc$/thes_de_CH_v2.don
 
 .ENDIF
 
