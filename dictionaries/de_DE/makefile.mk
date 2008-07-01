e4ff475460e74fbe37f0697af994c4fd9a425e6c
Vladimir Glazounov <vg@openoffice.org>
2008-07-01 14:22:23 +0000

    INTEGRATION: CWS native156 (1.12.12); FILE MERGED 2008/06/18 13:07:55 mba 1.12.12.2: #i90657#: create dictionary extensions 2008/06/18 12:43:52 mba 1.12.12.1: #i90657#: create extensions

--- dictionaries/de_DE/makefile.mk
+++ dictionaries/de_DE/makefile.mk
@@ -1,14 +1,14 @@
 #*************************************************************************
 #
 # DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
-# 
+#
 # Copyright 2008 by Sun Microsystems, Inc.
 #
 # OpenOffice.org - a multi-platform office productivity suite
 #
 # $RCSfile: makefile.mk,v $
 #
-# $Revision: 1.12 $
+# $Revision: 1.13 $
 #
 # This file is part of OpenOffice.org.
 #
@@ -29,42 +29,67 @@
 #
 #*************************************************************************
 
-PRJ = ..
+PRJ=..
 
-PRJNAME	= dictionaries
-TARGET	= dict_de_DE
+PRJNAME=dictionaries
+TARGET=dict-de
 
-#----- Settings ---------------------------------------------------------
+# --- Settings -----------------------------------------------------
 
-.INCLUDE : settings.mk
+.INCLUDE: settings.mk
+# it might be useful to have an extension wide include to set things
+# like the EXTNAME variable (used for configuration processing)
+# .INCLUDE :  $(PRJ)$/source$/<extension name>$/<extension_name>.pmk
 
 # --- Files --------------------------------------------------------
 
-.IF "$(DIC_ALL)$(DIC_DEDE)"!=""
+# name for uniq directory
+EXTENSIONNAME:=dict-de
+EXTENSION_ZIPNAME:=dict-de
 
-DIC2BIN= \
-    hyph_de_DE.dic \
-    README_hyph_de_DE.txt \
-    de_DE.aff \
-    de_DE.dic \
-    README_de_DE.txt
+# some other targets to be done
 
-.ENDIF
+# --- Extension packaging ------------------------------------------
 
-# --- Targets ------------------------------------------------------
+# just copy:
+COMPONENT_FILES= \
+    $(EXTENSIONDIR)$/COPYING \
+    $(EXTENSIONDIR)$/COPYING_OASIS \
+    $(EXTENSIONDIR)$/Copyright \
+    $(EXTENSIONDIR)$/de_DE.aff \
+    $(EXTENSIONDIR)$/de_DE.dic \
+    $(EXTENSIONDIR)$/hyph_de_DE.dic \
+    $(EXTENSIONDIR)$/INSTALL_de.txt \
+    $(EXTENSIONDIR)$/README_de_DE.txt \
+    $(EXTENSIONDIR)$/README_hyph_de_DE.txt \
+    $(EXTENSIONDIR)$/VERSION
 
+COMPONENT_CONFIGDEST=.
+COMPONENT_XCU= \
+    $(EXTENSIONDIR)$/dictionaries.xcu
+
+# disable fetching default OOo license text
+# CUSTOM_LICENSE=Copyright
+# override default license destination
+# PACKLICS= $(EXTENSIONDIR)$/registration$/$(CUSTOM_LICENSE)
+
+COMPONENT_ZIP:=$(PWD)$/thes_de_DE_v2.zip
+COMPONENT_UNZIP_FILES= \
+    $(EXTENSIONDIR)$/th_de_DE_v2.dat \
+    $(EXTENSIONDIR)$/th_de_DE_v2.idx
+
+# add own targets to packing dependencies (need to be done before
+# packing the xtension
+# EXTENSION_PACKDEPS=makefile.mk $(CUSTOM_LICENSE)
+EXTENSION_PACKDEPS=$(COMPONENT_UNZIP_FILES)
+
+# global settings for extension packing
+.INCLUDE : extension_pre.mk
 .INCLUDE : target.mk
-.INCLUDE : $(PRJ)$/util$/target.pmk
+# global targets for extension packing
+.INCLUDE : extension_post.mk
 
-#
-# add thesaurus for de_DE de_AT
-#
-.IF "$(DIC_ALL)$(DIC_DEDE)$(DIC_DEAT)"!=""
-
-ALLTAR : $(MISC)$/thes_de_DE_v2.don
-
-$(MISC)$/thes_de_DE_v2.don: thes_de_DE_v2.zip
-    cd $(BIN) && $(WRAPCMD) unzip -o $(PWD)$/thes_de_DE_v2.zip && $(TOUCH) ..$/misc$/thes_de_DE_v2.don
-
-.ENDIF
-
+.IF "$(COMPONENT_UNZIP_FILES)"!=""
+$(COMPONENT_UNZIP_FILES) : "$(COMPONENT_ZIP)"
+    cd $(EXTENSIONDIR) && unzip -o $< $(subst,$(EXTENSIONDIR)$/, $@)
+.ENDIF			# "$(COMPONENT_UNZIP_FILES)"!=""
