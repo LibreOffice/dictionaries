53dd79ee52597482164c8a4a6d0f9ff6673d8bc4
Vladimir Glazounov <vg@openoffice.org>
2008-07-01 14:31:02 +0000

    INTEGRATION: CWS native156 (1.3.12); FILE MERGED 2008/06/18 13:07:57 mba 1.3.12.2: #i90657#: create dictionary extensions 2008/06/18 12:43:56 mba 1.3.12.1: #i90657#: create extensions

--- dictionaries/hu_HU/makefile.mk
+++ dictionaries/hu_HU/makefile.mk
@@ -1,14 +1,14 @@
 #*************************************************************************
 #
 # DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
-# 
+#
 # Copyright 2008 by Sun Microsystems, Inc.
 #
 # OpenOffice.org - a multi-platform office productivity suite
 #
 # $RCSfile: makefile.mk,v $
 #
-# $Revision: 1.3 $
+# $Revision: 1.4 $
 #
 # This file is part of OpenOffice.org.
 #
@@ -29,30 +29,53 @@
 #
 #*************************************************************************
 
-PRJ = ..
+PRJ=..
 
-PRJNAME	= dictionaries
-TARGET	= dict_hu_HU
+PRJNAME=dictionaries
+TARGET=dict-hu
 
-#----- Settings ---------------------------------------------------------
+# --- Settings -----------------------------------------------------
 
-.INCLUDE : settings.mk
+.INCLUDE: settings.mk
+# it might be useful to have an extension wide include to set things
+# like the EXTNAME variable (used for configuration processing)
+# .INCLUDE :  $(PRJ)$/source$/<extension name>$/<extension_name>.pmk
 
 # --- Files --------------------------------------------------------
 
-.IF "$(DIC_ALL)$(DIC_HUHU)"!=""
+# name for uniq directory
+EXTENSIONNAME:=dict-hu
+EXTENSION_ZIPNAME:=dict-hu
 
-DIC2BIN= \
-    hu_HU.aff \
-    hu_HU.dic \
-    hyph_hu_HU.dic \
-    README_hu_HU.txt \
-    README_hyph_hu_HU.txt
+# some other targets to be done
 
-.ENDIF
+# --- Extension packaging ------------------------------------------
 
-# --- Targets ------------------------------------------------------
+# just copy:
+COMPONENT_FILES= \
+    $(EXTENSIONDIR)$/hu_HU.aff \
+    $(EXTENSIONDIR)$/hu_HU.dic \
+    $(EXTENSIONDIR)$/hyph_hu_HU.dic \
+    $(EXTENSIONDIR)$/README_hu_HU.txt \
+    $(EXTENSIONDIR)$/README_hyph_hu_HU.txt
 
+COMPONENT_CONFIGDEST=.
+COMPONENT_XCU= \
+    $(EXTENSIONDIR)$/dictionaries.xcu
+
+# disable fetching default OOo license text
+# CUSTOM_LICENSE=README_da_DK.txt
+# override default license destination
+# PACKLICS= $(EXTENSIONDIR)$/registration$/$(CUSTOM_LICENSE)
+
+# add own targets to packing dependencies (need to be done before
+# packing the xtension
+# EXTENSION_PACKDEPS=makefile.mk $(CUSTOM_LICENSE)
+EXTENSION_PACKDEPS=$(COMPONENT_UNZIP_FILES)
+
+# global settings for extension packing
+.INCLUDE : extension_pre.mk
 .INCLUDE : target.mk
-.INCLUDE : $(PRJ)$/util$/target.pmk
+# global targets for extension packing
+.INCLUDE : extension_post.mk
 
