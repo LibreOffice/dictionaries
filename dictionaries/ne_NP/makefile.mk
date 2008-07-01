22417035df207618df4c15814f610e3dee3e56c6
Vladimir Glazounov <vg@openoffice.org>
2008-07-01 14:34:42 +0000

    INTEGRATION: CWS native156 (1.3.12); FILE MERGED 2008/06/25 13:10:08 mba 1.3.12.4: wrong rules for idx 2008/06/25 12:44:09 mba 1.3.12.3: missing idx files for thesauri 2008/06/18 13:07:58 mba 1.3.12.2: #i90657#: create dictionary extensions 2008/06/18 12:43:57 mba 1.3.12.1: #i90657#: create extensions

--- dictionaries/ne_NP/makefile.mk
+++ dictionaries/ne_NP/makefile.mk
@@ -1,14 +1,14 @@
 #*************************************************************************
 #
 # DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
-# 
+#
 # Copyright 2008 by Sun Microsystems, Inc.
 #
 # OpenOffice.org - a multi-platform office productivity suite
 #
 # $RCSfile: makefile.mk,v $
 #
-# $Revision: 1.3 $
+# $Revision: 1.4 $
 #
 # This file is part of OpenOffice.org.
 #
@@ -29,38 +29,63 @@
 #
 #*************************************************************************
 
-PRJ = ..
+PRJ=..
 
-PRJNAME	= dictionaries
-TARGET	= dict_ne_NP
+PRJNAME=dictionaries
+TARGET=dict-ne
 
-#----- Settings ---------------------------------------------------------
+# --- Settings -----------------------------------------------------
 
-.INCLUDE : settings.mk
+.INCLUDE: settings.mk
+# it might be useful to have an extension wide include to set things
+# like the EXTNAME variable (used for configuration processing)
+# .INCLUDE :  $(PRJ)$/source$/<extension name>$/<extension_name>.pmk
 
 # --- Files --------------------------------------------------------
 
-.IF "$(DIC_ALL)$(DIC_NENP)"!=""
+# name for uniq directory
+EXTENSIONNAME:=dict-ne
+EXTENSION_ZIPNAME:=dict-ne
 
-DIC2BIN= \
-    ne_NP.aff \
-    ne_NP.dic \
-    README_ne_NP.txt \
-    README_th_ne_NP_v2.txt
+# some other targets to be done
 
-.ENDIF
+# --- Extension packaging ------------------------------------------
 
-# --- Targets ------------------------------------------------------
+# just copy:
+COMPONENT_FILES= \
+    $(EXTENSIONDIR)$/ne_NP.aff \
+    $(EXTENSIONDIR)$/ne_NP.dic \
+    $(EXTENSIONDIR)$/README_ne_NP.txt \
+    $(EXTENSIONDIR)$/README_th_ne_NP_v2.txt
 
+COMPONENT_CONFIGDEST=.
+COMPONENT_XCU= \
+    $(EXTENSIONDIR)$/dictionaries.xcu
+
+# disable fetching default OOo license text
+# CUSTOM_LICENSE=WordNet_license.txt
+# override default license destination
+# PACKLICS= $(EXTENSIONDIR)$/registration$/$(CUSTOM_LICENSE)
+
+COMPONENT_ZIP:=$(PWD)$/th_ne_NP_v2.zip
+COMPONENT_UNZIP_FILES= \
+    $(EXTENSIONDIR)$/th_ne_NP_v2.dat 
+
+# add own targets to packing dependencies (need to be done before
+# packing the xtension
+# EXTENSION_PACKDEPS=makefile.mk $(CUSTOM_LICENSE)
+EXTENSION_PACKDEPS=$(COMPONENT_UNZIP_FILES) $(EXTENSIONDIR)$/th_ne_NP_v2.idx
+
+# global settings for extension packing
+.INCLUDE : extension_pre.mk
 .INCLUDE : target.mk
-.INCLUDE : $(PRJ)$/util$/target.pmk
+# global targets for extension packing
+.INCLUDE : extension_post.mk
 
-.IF "$(DIC_ALL)$(DIC_NENP)"!=""
+.IF "$(COMPONENT_UNZIP_FILES)"!=""
+$(COMPONENT_UNZIP_FILES) : "$(COMPONENT_ZIP)"
+    cd $(EXTENSIONDIR) && unzip -o $< $(subst,$(EXTENSIONDIR)$/, $@)
+.ENDIF			# "$(COMPONENT_UNZIP_FILES)"!=""
 
-ALLTAR : $(MISC)$/th_ne_NP_v2.don
-
-$(MISC)$/th_ne_NP_v2.don: th_ne_NP_v2.zip
-    +cd $(BIN) && $(WRAPCMD) unzip -o $(PWD)$/th_ne_NP_v2.zip
-    +$(PERL) $(PRJ)$/util$/th_gen_idx.pl -o $(BIN)$/th_ne_NP_v2.idx <$(BIN)$/th_ne_NP_v2.dat && $(TOUCH) $(MISC)$/th_ne_NP_v2.don
-
-.ENDIF			# "$(DIC_ALL)$(DIC_NENP)"!=""
+$(EXTENSIONDIR)$/th_ne_NP_v2.idx : "$(EXTENSIONDIR)$/th_ne_NP_v2.dat"
+        $(PERL) $(PRJ)$/util$/th_gen_idx.pl -o $(EXTENSIONDIR)$/th_ne_NP_v2.idx <$(EXTENSIONDIR)$/th_ne_NP_v2.dat
