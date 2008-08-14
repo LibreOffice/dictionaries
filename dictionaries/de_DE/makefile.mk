da370cee690c9941857f6b7d7a3d3fa101730625
Kurt Zenker <kz@openoffice.org>
2008-08-14 13:23:39 +0000

    INTEGRATION: CWS sw30bf12_DEV300 (1.13.8); FILE MERGED 2008/08/07 17:04:25 mba 1.13.8.1: prevent adding of licence files in 73 languages

--- dictionaries/de_DE/makefile.mk
+++ dictionaries/de_DE/makefile.mk
@@ -8,7 +8,7 @@
 #
 # $RCSfile: makefile.mk,v $
 #
-# $Revision: 1.13 $
+# $Revision: 1.14 $
 #
 # This file is part of OpenOffice.org.
 #
@@ -54,7 +54,6 @@ EXTENSION_ZIPNAME:=dict-de
 # just copy:
 COMPONENT_FILES= \
     $(EXTENSIONDIR)$/COPYING \
-    $(EXTENSIONDIR)$/COPYING_OASIS \
     $(EXTENSIONDIR)$/Copyright \
     $(EXTENSIONDIR)$/de_DE.aff \
     $(EXTENSIONDIR)$/de_DE.dic \
@@ -69,9 +68,9 @@ COMPONENT_XCU= \
     $(EXTENSIONDIR)$/dictionaries.xcu
 
 # disable fetching default OOo license text
-# CUSTOM_LICENSE=Copyright
+CUSTOM_LICENSE=COPYING_OASIS
 # override default license destination
-# PACKLICS= $(EXTENSIONDIR)$/registration$/$(CUSTOM_LICENSE)
+PACKLICS= $(EXTENSIONDIR)$/$(CUSTOM_LICENSE)
 
 COMPONENT_ZIP:=$(PWD)$/thes_de_DE_v2.zip
 COMPONENT_UNZIP_FILES= \
@@ -81,7 +80,7 @@ COMPONENT_UNZIP_FILES= \
 # add own targets to packing dependencies (need to be done before
 # packing the xtension
 # EXTENSION_PACKDEPS=makefile.mk $(CUSTOM_LICENSE)
-EXTENSION_PACKDEPS=$(COMPONENT_UNZIP_FILES)
+EXTENSION_PACKDEPS=$(COMPONENT_UNZIP_FILES) $(COMPONENT_FILES)
 
 # global settings for extension packing
 .INCLUDE : extension_pre.mk
