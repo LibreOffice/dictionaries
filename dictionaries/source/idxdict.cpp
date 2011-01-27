0c8c81d4a463ccf1cfd60445e1dfcf218e9db4fb
Steven Butler <stevenb@kjross.com.au>
2011-01-27 17:58:51 +0000

    create native code dictionary indexer

---
+++ dictionaries/source/idxdict.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <map>
+#include <stdlib.h>
+#include <string.h>
+
+static const int MAXLINE = 1024*64;
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+        if (argc != 3 || strcmp(argv[1],"-o"))
+        {
+                cout << "Usage: idxdict -o outputfile < input\n";
+                ::exit(99);
+        }
+        // This call improves performance by approx 5x
+        cin.sync_with_stdio(false);
+
+        const char * outputFile(argv[2]);
+        char inputBuffer[MAXLINE];
+        multimap<string, size_t> entries;
+        multimap<string,size_t>::iterator ret(entries.begin());
+
+        int line(1);
+        cin.getline(inputBuffer, MAXLINE);
+        const string encoding(inputBuffer);
+        size_t currentOffset(encoding.size()+1);
+        while (true)
+        {
+                // Extract the next word, but not the entry count
+                cin.getline(inputBuffer, MAXLINE, '|');
+
+                if (cin.eof()) break;
+
+                string word(inputBuffer);
+                ret = entries.insert(ret, pair<string, size_t>(word, currentOffset));
+                currentOffset += word.size() + 1;
+                // Next is the entry count
+                cin.getline(inputBuffer, MAXLINE);
+                if (!cin.good())
+                {
+                        cerr << "Unable to read entry - insufficient buffer?.\n";
+                        exit(99);
+                }
+                currentOffset += strlen(inputBuffer)+1;
+                int entryCount(strtol(inputBuffer, NULL, 10));
+                for (int i(0); i < entryCount; ++i)
+                {
+                        cin.getline(inputBuffer, MAXLINE);
+                        currentOffset += strlen(inputBuffer)+1;
+                        ++line;
+                }
+        }
+
+        // Use binary mode to prevent any translation of LF to CRLF on Windows
+        ofstream outputStream(outputFile, ios_base::binary| ios_base::trunc|ios_base::out);
+        if (!outputStream.is_open())
+        {
+                cerr << "Unable to open output file " << outputFile << endl;
+                ::exit(99);
+        }
+
+        cout << outputFile << endl;
+
+        outputStream << encoding << '\n' << entries.size() << '\n';
+
+        for (multimap<string, size_t>::const_iterator ii(entries.begin());
+                ii != entries.end();
+                ++ii
+        )
+        {
+                outputStream << ii->first << '|' << ii->second << '\n';
+        }
+}
+
