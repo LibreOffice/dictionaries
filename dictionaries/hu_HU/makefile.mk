f43414bb4f11b1cab45dac0cfd14315f9363b2f1
Andras Timar <atimar@suse.com>
2012-07-15 18:07:50 +0200

    re-add help.tree to Hungarian dict help (hu only)
    Change-Id: Ic1d9b96a6096e01499e31f5d32a0e8ca56ddc97a

--- dictionaries/hu_HU/makefile.mk
+++ dictionaries/hu_HU/makefile.mk
@@ -59,6 +59,8 @@ COMPONENT_FILES= \
     $(EXTENSIONDIR)$/README_hyph_hu_HU.txt \
     $(EXTENSIONDIR)$/README_th_hu_HU_v2.txt \
     $(EXTENSIONDIR)$/th_hu_HU_v2.dat \
+    $(EXTENSIONDIR)$/help/en/help.tree \
+    $(EXTENSIONDIR)$/help/hu/help.tree \
     $(EXTENSIONDIR)$/help/hu/org.openoffice.hu.hunspell.dictionaries/page1.xhp \
     $(EXTENSIONDIR)$/help/hu/org.openoffice.hu.hunspell.dictionaries/fsfhu.png \
     $(EXTENSIONDIR)$/help/hu/org.openoffice.hu.hunspell.dictionaries/szinonima.png \
