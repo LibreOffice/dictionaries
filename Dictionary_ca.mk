05601c2395351070ebb4c690a207faa97bcb6ed7
Andras Timar <atimar@suse.com>
2013-02-22 10:03:18 +0100

    fdo#61139 update Catalan dictionaries
    Change-Id: If4595c3d94803b22e1f0b635a623260715696230
    Reviewed-on: https://gerrit.libreoffice.org/2331
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>
    Tested-by: Caolán McNamara <caolanm@redhat.com>

--- Dictionary_ca.mk
+++ Dictionary_ca.mk
@@ -12,21 +12,12 @@ $(eval $(call gb_Dictionary_Dictionary,dict-ca,dictionaries/ca))
 $(eval $(call gb_Dictionary_add_root_files,dict-ca,\
 	dictionaries/ca/ca.aff \
 	dictionaries/ca/ca.dic \
-	dictionaries/ca/dict_LICENSES-en.txt \
-	dictionaries/ca/dict_LLICENCIES-ca.txt \
 	dictionaries/ca/hyph_ca.dic \
-	dictionaries/ca/hyph_LICENSES-en.txt \
-	dictionaries/ca/hyph_LLICENCIES-ca.txt \
-	dictionaries/ca/hyph_release-note_ca.txt \
-	dictionaries/ca/hyph_release-note_en.txt \
+	dictionaries/ca/SC_42x42.png \
+	dictionaries/ca/LICENCES-fr.txt \
+	dictionaries/ca/LICENCIAS-es.txt \
 	dictionaries/ca/LICENSES-en.txt \
 	dictionaries/ca/LLICENCIES-ca.txt \
-	dictionaries/ca/README_th_ca_ES_v3.txt \
-	dictionaries/ca/th_LICENCES-fr.txt \
-	dictionaries/ca/th_LICENCIAS-es.txt \
-	dictionaries/ca/th_LICENSES-en.txt \
-	dictionaries/ca/th_LLICENCIES-ca.txt \
-	dictionaries/ca/th_release_note-ca.txt \
 ))
 
 $(eval $(call gb_Dictionary_add_thesauri,dict-ca,\
