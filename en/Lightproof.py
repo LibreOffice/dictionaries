LibreOffice/dictionaries

commit 080424980483bcc29a3556cf1a681c45cd5c50fc

Lightproof: restore paragraph based proofreading
Instead of the sentences suggested by the incomplete sentence
boundary detection of LibreOffice (enabled in version 4), Lightproof
works on full paragraph text again, allowing more precise analysis of
the text.

See http://nabble.documentfoundation.org/Grammar-checker-Undocumented-change-in-the-API-for-LO-4-td4030639.html

Change-Id: Ifc967e5b3f27da5b0edeebdb73b8f0b97c938280

en/Lightproof.py
@@ -77,6 +77,11 @@ class Lightproof( unohelper.Base, XProofreader, XServiceInfo, XServiceName, XSer
         aRes.aProperties = ()
         aRes.xProofreader = self
         aRes.aErrors = ()
+        # PATCH FOR LO 4
+        # Fix for http://nabble.documentfoundation.org/Grammar-checker-Undocumented-change-in-the-API-for-LO-4-td4030639.html
+        if nStartOfSentencePos != 0:
+            return aRes
+        aRes.nStartOfNextSentencePosition = len(rText)
         if len(rProperties) > 0 and rProperties[0].Name == "Update":
             try:
                 import lightproof_compile_en

hu_HU/Lightproof.py
@@ -77,6 +77,12 @@ class Lightproof( unohelper.Base, XProofreader, XServiceInfo, XServiceName, XSer
         aRes.aProperties = ()
         aRes.xProofreader = self
         aRes.aErrors = ()
+        # PATCH FOR LO 4
+        # Fix for http://nabble.documentfoundation.org/Grammar-checker-Undocumented-change-in-the-API-for-LO-4-td4030639.html
+        if nStartOfSentencePos != 0:
+            return aRes
+        aRes.nStartOfNextSentencePosition = len(rText)
+        # END OF PATCH
         if len(rProperties) > 0 and rProperties[0].Name == "Update":
             try:
                 import lightproof_compile_hu_HU

pt_BR/Lightproof.py
@@ -77,6 +77,12 @@ class Lightproof( unohelper.Base, XProofreader, XServiceInfo, XServiceName, XSer
         aRes.aProperties = ()
         aRes.xProofreader = self
         aRes.aErrors = ()
+        # PATCH FOR LO 4
+        # Fix for http://nabble.documentfoundation.org/Grammar-checker-Undocumented-change-in-the-API-for-LO-4-td4030639.html
+        if nStartOfSentencePos != 0:
+            return aRes
+        aRes.nStartOfNextSentencePosition = len(rText)
+        # END OF PATCH
         if len(rProperties) > 0 and rProperties[0].Name == "Update":
             try:
                 import lightproof_compile_pt_BR

ru_RU/Lightproof.py
@@ -77,6 +77,12 @@ class Lightproof( unohelper.Base, XProofreader, XServiceInfo, XServiceName, XSer
         aRes.aProperties = ()
         aRes.xProofreader = self
         aRes.aErrors = ()
+        # PATCH FOR LO 4
+        # Fix for http://nabble.documentfoundation.org/Grammar-checker-Undocumented-change-in-the-API-for-LO-4-td4030639.html
+        if nStartOfSentencePos != 0:
+            return aRes
+        aRes.nStartOfNextSentencePosition = len(rText)
+        # END OF PATCH
         if len(rProperties) > 0 and rProperties[0].Name == "Update":
             try:
                 import lightproof_compile_ru_RU

commit 89579ddf7d8e8952e65159be11dbc54610843f73

Lightproof: do not return invalid nBehindEndOfSentencePosition
For a paragraph with a space at the end, this assertion is triggered:

linguistic/source/gciterator.cxx:162: lcl_SkipWhiteSpaces: illegal
arguments

Cause is the loop in doProofReading that gets the last character of the
paragraph and increments nStartOfNextSentencePosition even if it's
already at end of the text.

(regression from 080424980483bcc29a3556cf1a681c45cd5c50fc)

Change-Id: I58bd10d168f75f9d365cb33c0ade770750ff57ef

en/Lightproof.py
@@ -121,7 +121,7 @@ class Lightproof( unohelper.Base, XProofreader, XServiceInfo, XServiceName, XSer
                 if 'PYUNO_LOGLEVEL' in os.environ:
                     print(traceback.format_exc())
 
-        l = rText[nSuggestedSentenceEndPos:nSuggestedSentenceEndPos+1]
+        l = rText[aRes.nStartOfNextSentencePosition:aRes.nStartOfNextSentencePosition+1]
         while l == " ":
             aRes.nStartOfNextSentencePosition = aRes.nStartOfNextSentencePosition + 1
             l = rText[aRes.nStartOfNextSentencePosition:aRes.nStartOfNextSentencePosition+1]