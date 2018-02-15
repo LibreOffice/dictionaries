411734ee6f2eeee7b7a07546f211bd036493729e
Andika Triwidada <andika@gmail.com>
2018-02-15 03:52:58 +0000

    Add Indonesian dictionary
    Change-Id: I1368bccffe9f192de4d73c5f658d4f864e3374da

---
+++ Dictionary_id.mk
@@ -0,0 +1,19 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+$(eval $(call gb_Dictionary_Dictionary,dict-id,dictionaries/id))
+
+$(eval $(call gb_Dictionary_add_root_files,dict-id,\
+	dictionaries/id/LICENSE \
+	dictionaries/id/id_ID.aff \
+	dictionaries/id/id_ID.dic \
+	dictionaries/id/README.md \
+))
+
+# vim: set noet sw=4 ts=4:
