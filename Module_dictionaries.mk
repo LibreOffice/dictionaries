169841406aed65929dbbfb3e2f57d1997807e0b6
Andras Timar <andras.timar@collabora.com>
2023-05-24 08:15:10 +0200

    On Linux dictionaries are packaged separately
    Change-Id: I604a0f12bdbedc0f35a27ead0f995e9dd3a27d8b

--- Module_dictionaries.mk
+++ Module_dictionaries.mk
@@ -30,9 +30,9 @@ $(eval $(call gb_Module_add_l10n_targets,dictionaries,\
 	Dictionary_ckb \
 	Dictionary_cs \
 	Dictionary_da \
-	$(if $(MPL_SUBSET),,Dictionary_de) \
+	$(if $(and $(MPL_SUBSET), $(filter-out LINUX,$(OS))),,Dictionary_de) \
 	Dictionary_el \
-	$(if $(MPL_SUBSET),,Dictionary_en) \
+	$(if $(and $(MPL_SUBSET), $(filter-out LINUX,$(OS))),,Dictionary_en) \
 	Dictionary_eo \
 	Dictionary_es \
 	Dictionary_et \
@@ -48,7 +48,7 @@ $(eval $(call gb_Module_add_l10n_targets,dictionaries,\
 	Dictionary_hu \
 	Dictionary_id \
 	Dictionary_is \
-	$(if $(MPL_SUBSET),,Dictionary_it) \
+	$(if $(and $(MPL_SUBSET), $(filter-out LINUX,$(OS))),,Dictionary_it) \
 	Dictionary_kmr-Latn \
 	Dictionary_ko-KR \
 	Dictionary_lo \
