b1beb866b76156410f80a79b4d1b9ed4d2fa528c
Caolán McNamara <caolan.mcnamara@collabora.com>
2023-07-01 21:55:34 +0100

    tdf#156101 add en-IL alongside the other English locales
    Change-Id: Iae8812e6bd0f10c2ecbf2f985d465e8fd1c4993e
    Reviewed-on: https://gerrit.libreoffice.org/c/dictionaries/+/153852
    Tested-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Reviewed-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    (cherry picked from commit 3fdd5e2b1d652e4ee7212c934d916971fc74a203)
    Reviewed-on: https://gerrit.libreoffice.org/c/dictionaries/+/153845
    Tested-by: Christian Lohmaier <lohmaier+LibreOffice@googlemail.com>
    Reviewed-by: Christian Lohmaier <lohmaier+LibreOffice@googlemail.com>

--- en/pythonpath/lightproof_impl_en.py
+++ en/pythonpath/lightproof_impl_en.py
@@ -5,7 +5,7 @@ from com.sun.star.beans import PropertyValue
 
 pkg = "en"
 lang = "en"
-locales = {'en-GB': ['en', 'GB', ''], 'en-ZW': ['en', 'ZW', ''], 'en-PH': ['en', 'PH', ''], 'en-TT': ['en', 'TT', ''], 'en-BZ': ['en', 'BZ', ''], 'en-NA': ['en', 'NA', ''], 'en-IE': ['en', 'IE', ''], 'en-GH': ['en', 'GH', ''], 'en-US': ['en', 'US', ''], 'en-IN': ['en', 'IN', ''], 'en-BS': ['en', 'BS', ''], 'en-JM': ['en', 'JM', ''], 'en-AU': ['en', 'AU', ''], 'en-NZ': ['en', 'NZ', ''], 'en-ZA': ['en', 'ZA', ''], 'en-CA': ['en', 'CA', '']}
+locales = {'en-GB': ['en', 'GB', ''], 'en-ZW': ['en', 'ZW', ''], 'en-PH': ['en', 'PH', ''], 'en-TT': ['en', 'TT', ''], 'en-BZ': ['en', 'BZ', ''], 'en-NA': ['en', 'NA', ''], 'en-IE': ['en', 'IE', ''], 'en-IL': ['en', 'IL', ''], 'en-GH': ['en', 'GH', ''], 'en-US': ['en', 'US', ''], 'en-IN': ['en', 'IN', ''], 'en-BS': ['en', 'BS', ''], 'en-JM': ['en', 'JM', ''], 'en-AU': ['en', 'AU', ''], 'en-NZ': ['en', 'NZ', ''], 'en-ZA': ['en', 'ZA', ''], 'en-CA': ['en', 'CA', '']}
 version = "0.4.3"
 author = "László Németh"
 name = "Lightproof grammar checker (English)"
