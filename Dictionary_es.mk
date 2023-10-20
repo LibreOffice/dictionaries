LibreOffice/dictionaries

commit c1ddcf602ba97f8e99383f34e8027074d8193f61

Updating RLA-ES Spanish spellcheker to v2.8.
Change-Id: I749268a6e38e65e86633f39ea14710893cb6b468
Reviewed-on: https://gerrit.libreoffice.org/c/dictionaries/+/156268
Reviewed-by: Adolfo Jayme Barrientos <fitojb@ubuntu.com>
Reviewed-by: Thorsten Behrens <thorsten.behrens@allotropia.de>
Tested-by: Thorsten Behrens <thorsten.behrens@allotropia.de>

Dictionary_es.mk
@@ -9,9 +9,11 @@
 
 $(eval $(call gb_Dictionary_Dictionary,dict-es,dictionaries/es))
 
+$(eval $(call gb_Dictionary_add_files,dict-es,images,\
+    dictionaries/es/RLA-ES.png \
+))
+
 $(eval $(call gb_Dictionary_add_root_files,dict-es,\
-	dictionaries/es/es.aff \
-	dictionaries/es/es.dic \
 	dictionaries/es/es_AR.aff \
 	dictionaries/es/es_AR.dic \
 	dictionaries/es/es_BO.aff \
@@ -63,7 +65,6 @@ $(eval $(call gb_Dictionary_add_root_files,dict-es,\
 	dictionaries/es/README_hunspell_es.txt \
 	dictionaries/es/README_hyph_es.txt \
 	dictionaries/es/README_th_es.txt \
-	dictionaries/es/RLA-ES.png \
 	dictionaries/es/LICENSE.md \
 ))
 

es/description.xml
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <description xmlns="http://openoffice.org/extensions/description/2006" xmlns:d="http://openoffice.org/extensions/description/2006"  xmlns:xlink="http://www.w3.org/1999/xlink">
 
-  <version value="2.6" />
+  <version value="2.8" />
   <identifier value="spanish.es.dicts.from.rla-es"/>
 
   <display-name>

es/dictionaries.xcu
@@ -2,17 +2,6 @@
 <oor:component-data xmlns:oor="http://openoffice.org/2001/registry" xmlns:xs="http://www.w3.org/2001/XMLSchema" oor:name="Linguistic" oor:package="org.openoffice.Office">
  <node oor:name="ServiceManager">
     <node oor:name="Dictionaries">
-        <node oor:name="HunSpellDic_es" oor:op="fuse">
-            <prop oor:name="Locations" oor:type="oor:string-list">
-                <value>%origin%/es.aff %origin%/es.dic</value>
-            </prop>
-            <prop oor:name="Format" oor:type="xs:string">
-                <value>DICT_SPELL</value>
-            </prop>
-            <prop oor:name="Locales" oor:type="oor:string-list">
-                <value>es es-AR es-BO es-CL es-CO es-CR es-CU es-DO es-EC es-ES es-GQ es-GT es-HN es-MX es-NI es-PA es-PE es-PH es-PR es-PY es-SV es-US es-UY es-VE</value>
-            </prop>
-        </node>
         <node oor:name="HunSpellDic_es_AR" oor:op="fuse">
             <prop oor:name="Locations" oor:type="oor:string-list">
                 <value>%origin%/es_AR.aff %origin%/es_AR.dic</value>
@@ -274,7 +263,7 @@
                 <value>DICT_HYPH</value>
             </prop>
             <prop oor:name="Locales" oor:type="oor:string-list">
-                <value>es es-AR es-BO es-CL es-CO es-CR es-CU es-DO es-EC es-ES es-GQ es-GT es-HN es-MX es-NI es-PA es-PE es-PH es-PR es-PY es-SV es-US es-UY es-VE</value>
+                <value>es-AR es-BO es-CL es-CO es-CR es-CU es-DO es-EC es-ES es-GQ es-GT es-HN es-MX es-NI es-PA es-PE es-PH es-PR es-PY es-SV es-US es-UY es-VE</value>
             </prop>
         </node>
         <node oor:name="ThesDic_es" oor:op="fuse">
@@ -285,7 +274,7 @@
                 <value>DICT_THES</value>
             </prop>
             <prop oor:name="Locales" oor:type="oor:string-list">
-                <value>es es-AR es-BO es-CL es-CO es-CR es-CU es-DO es-EC es-ES es-GQ es-GT es-HN es-MX es-NI es-PA es-PE es-PH es-PR es-PY es-SV es-US es-UY es-VE</value>
+                <value>es-AR es-BO es-CL es-CO es-CR es-CU es-DO es-EC es-ES es-GQ es-GT es-HN es-MX es-NI es-PA es-PE es-PH es-PR es-PY es-SV es-US es-UY es-VE</value>
             </prop>
         </node>
     </node>

es/es_AR.dic
@@ -1,4 +1,4 @@
-56304
+56376
 ABS
 ADSL
 Aberastain
@@ -126,6 +126,7 @@ Burundi
 Bután
 Bélgica
 CD
+COVID
 Cafayate
 Caicos
 Caimancito
@@ -3827,6 +3828,7 @@ alumbre/S
 aluminato/S
 aluminio/S
 aluminita/S
+aluminosis
 alumnado/S
 alumno/GS
 alunado/GS
@@ -4289,6 +4291,7 @@ anadear/RED
 anadina/S
 anadino/S
 anadiplosis
+anaeróbico/GS
 anafalla/S
 anafe/S
 anafilaxia/S
@@ -5536,6 +5539,8 @@ archivar/REDÀÙ
 archivero/GS
 archivista/S
 archivo/S
+archivología/S
+archivístico/GS
 arché
 arcilla/S
 arcillar/RED
@@ -6783,7 +6788,6 @@ aturullar/RED
 atusador/GS
 atusar/REDÂ
 atávico/SG
-atípico/GS
 atómico/sSG
 atónito/SG
 atópico/GS
@@ -6947,6 +6951,7 @@ autoritarismo/S
 autorización/hS
 autorizadamente
 autorizado/SG
+autorizador/GS
 autorizamiento/S
 autorizar/REDÀÁÂÄÅÆÚT
 autorregular/REDA
@@ -7963,6 +7968,7 @@ basura/S
 basurear/RED
 basurero/GS
 basurita/S
+basáltico/GS
 basílica/S
 bata/H
 bata/S
@@ -8306,6 +8312,7 @@ bibliología
 bibliometría
 biblioteca/S
 bibliotecario/GS
+biblioteconomía/S
 bicampeonato/S
 bicentenario/S
 bicha/S
@@ -9112,6 +9119,7 @@ bribar
 bribonada/S
 bribonear/RED
 bribón/GS
+bricbarca/S
 bricolaje/S
 brida/S
 briega/S
@@ -9478,9 +9486,11 @@ bóvido/SG
 bóxer/S
 búbalo/S
 búfalo/S
+búfer/S
 búho/S
 búlgaro/GS
 búlgaro/S
+búnker/S
 búsqueda/S
 ca
 cabal
@@ -12355,6 +12365,7 @@ cicatrizativo/GS
 cicerone/S
 ciceroniano/GS
 cicerón/S
+ciclable/S
 ciclamor/S
 ciclar/RED
 ciclismo/S
@@ -13183,6 +13194,7 @@ colmo/SG
 colocado/GS
 colocar/REDÀÁÃÅÆÇÌÍÛñòôøAÙ
 colocasia/S
+colocón/S
 colodión/S
 colodra/S
 colodrillo/S
@@ -13483,6 +13495,7 @@ compensar/REDÀÁÂÄÆñ
 compensativo/GS
 compensatorio/GS
 competencia/kS
+competencial/S
 competente/S
 competente/kS
 competentemente
@@ -14234,6 +14247,7 @@ construcción/pS
 constructiva/p
 constructivismo
 constructivo/pSG
+constructo/S
 constructor/SG
 construir/IRDÀÁÂÈ
 consubstancialidad/S
@@ -14354,6 +14368,7 @@ continuamente
 continuar/IRDÀÂÄ
 continuativo/GS
 continuidad/S
+continuista/S
 continuo
 continuo/S
 continuo/kGSh
@@ -14632,6 +14647,7 @@ convincente/S
 convincentemente
 convite/S
 conviviente/S
+convivio/S
 convivir/RED
 convocador/GS
 convocante/S
@@ -14855,6 +14871,7 @@ coronamiento/S
 coronar/REDÀÌñ
 coronaria/S
 coronario/GS
+coronavirus
 corondel/S
 coronel/S
 coronela/S
@@ -15280,6 +15297,7 @@ crieis
 crimen/S
 criminal/S
 criminalidad/S
+criminalizar/REDÀÁÄÌÙ
 criminalmente
 criminar/RED
 criminoso/GS
@@ -15448,8 +15466,9 @@ crédulo/kSG
 cría/pS
 crío/GS
 crítica/S
-críticamente
+críticamente/a
 crítico/GS
+crítico/GSa
 crómico/SG
 crónica/S
 crónico/SG
@@ -15956,6 +15975,7 @@ cámaro/S
 cámbaro/S
 cámbrica/n
 cámbrico/nGS
+cáncamo/S
 cáncer/S
 cándidamente
 cándido/GS
@@ -16085,6 +16105,7 @@ dadas
 dadivar
 dadivosidad/S
 dadivoso/GS
+dadle
 dadles
 dadme
 dado
@@ -16403,6 +16424,7 @@ deflación/S
 deflagración/S
 deflagrador/S
 deflagrador/SG
+deflagrante/cS
 deflagrar/RED
 deflegmar/RED
 deflexión/S
@@ -16577,6 +16599,7 @@ demolición/S
 demonche/S
 demonio/S
 demonización/S
+demonizar/RED
 demonología/S
 demonológico/GS
 demonomancia/S
@@ -16769,7 +16792,9 @@ derivativo/GS
 dermalgia/S
 dermatitis
 dermatoesqueleto/S
+dermatología/S
 dermatosis
+dermatólogo/GS
 dermesto/S
 dermis
 dermitis
@@ -17340,7 +17365,7 @@ deschavetar/RED
 deschuponar/RED
 descifrable/kS
 descifrador/GS
-descifrar/REDÀÂ
+descifrar/REDÀÂP
 descimbramiento/S
 descimbrar/RED
 descimentar/IRD
@@ -17479,7 +17504,7 @@ descuajar/RED
 descuajaringar/RED
 descuajeringar/RED
 descuartizamiento/S
-descuartizar/REDÀÁÄ
+descuartizar/REDÀÁÄÌ
 descubiertamente
 descubierto/GS
 descubridero/S
@@ -17689,7 +17714,7 @@ desenfreno/S
 desenfundar/REDÀ
 desenfurecer/IRD
 desenfurruñar/RED
-desenganchar/REDñ
+desenganchar/REDÀñ
 desengarrafar
 desengarzar/RED
 desengastar/RED
@@ -19105,6 +19130,7 @@ dinamitar/REDÀ
 dinamitazo/S
 dinamitero/GS
 dinamización/S
+dinamizador/GS
 dinamizar/REDÀ
 dinamo/S
 dinamómetro/S
@@ -21645,6 +21671,7 @@ endurecer/IRDÀÄÅò
 endurecimiento/S
 endémico/SG
 ene
+ene/S
 enea/S
 eneal/S
 eneasílabo/GS
@@ -21783,7 +21810,7 @@ engallar/RED
 engalle/S
 enganchador/GS
 enganchamiento/S
-enganchar/REDÄñô
+enganchar/REDÀÄñô
 enganche/S
 engandujar/RED
 engarabatar/RED
@@ -23412,6 +23439,7 @@ escénico/GS
 escéptico/GS
 escúter/S
 esdrujulizar/RED
+esdrújulo/GS
 ese/G
 ese/GS
 ese/S
@@ -23831,6 +23859,7 @@ esposa/S
 esposar/REDÀÂ
 esposo/GS
 espray
+esprint
 espráis
 espuela/S
 espuerta/S
@@ -24440,6 +24469,7 @@ estulticia/S
 estulto/SG
 estuosidad/S
 estuoso/SG
+estupa/S
 estupefacción/S
 estupefaciente/S
 estupefactivo/GS
@@ -25136,6 +25166,7 @@ exótico/SG
 eyaculación/S
 eyacular/REDÁ
 ezquerdear/RED
+eñe/S
 eólico/GS
 eón/S
 fa
@@ -25315,6 +25346,7 @@ familiarmente
 famosamente
 famoso/GS
 famélico/GS
+fan/S
 fanal/S
 fanatismo/S
 fanatizador/GS
@@ -26786,6 +26818,7 @@ fálico/GS
 fárfara/S
 fármaco/S
 fárrago/S
+fático/SG
 fécula/S
 félido/GS
 félido/S
@@ -27143,6 +27176,7 @@ garra/S
 garrafa/S
 garrafal/S
 garrafiñar/RED
+garrafón/S
 garramar/RED
 garrancha/S
 garrancho/SH
@@ -27453,7 +27487,7 @@ gesticulantemente
 gesticular/RED
 gesticular/S
 gesticuloso/GS
-gestionar/REDÀÂ
+gestionar/REDÁÀÂ
 gestión/fSkd
 gesto/S
 gestor/GS
@@ -27771,6 +27805,7 @@ graduando/GS
 graduar/IRDÁÂñT
 grafeno/S
 graficar/RED
+grafitero/GS
 grafiti/S
 grafito/S
 grafo/S
@@ -29970,6 +30005,7 @@ húmedo/GS
 húmero/S
 húngaro/S
 húngaro/SG
+i
 ib
 iba
 iban
@@ -30617,6 +30653,7 @@ indigesto/SG
 indignación/S
 indignadísimo/GS
 indignamente
+indignante/S
 indignar/REDÁÂñ
 indignidad/S
 indilgar/RED
@@ -30793,6 +30830,7 @@ infirmar/RED
 inflacionario/GS
 inflacionista/S
 inflación/S
+inflador/GS
 inflamable/kS
 inflamación/S
 inflamador/GS
@@ -31435,7 +31473,7 @@ invenciblemente
 invención/S
 invenir
 inventar/REDÀÁÂÃÄÅÆÍ
-inventariar/IRDÀ
+inventariar/IRDÀT
 inventario/S
 inventiva/S
 inventivo/SG
@@ -32311,6 +32349,7 @@ juégalo
 juégalos
 jábega/S
 jácara/S
+jáquer/S
 jáquima/S
 jíbaro/GS
 jíbaro/SG
@@ -32338,6 +32377,7 @@ keniano/GS
 keniata/S
 kermes
 ketchup/S
+kg
 kiliárea/S
 kilo/S
 kilociclo/S
@@ -32852,6 +32892,7 @@ lazdrar
 lazo/Sa
 lazrar
 lazulita/S
+laísmo/S
 lañar/RED
 laúd/S
 ldo/G
@@ -33147,6 +33188,7 @@ lezna/S
 lezne/GS
 leída/S
 leído/GS
+leísmo/S
 leña/S
 leñador/GS
 leño/S
@@ -33842,6 +33884,7 @@ lozanear/RED
 lozanecer/IRD
 lozano/GS
 lozanía/S
+loísmo/S
 ltdo/G
 lubigante/S
 lubina/S
@@ -35499,6 +35542,7 @@ mecánicamente
 mecánico/GS
 mecánico/SG
 medalla/SU
+medallista/S
 medanoso/GS
 medellinense/S
 media/S
@@ -36209,7 +36253,7 @@ miera/S
 mierda/S
 mierla/S
 mierra/S
-mies
+mies/S
 miga/Sa
 migaja/S
 migajada/S
@@ -36525,6 +36569,7 @@ mizo/GS
 mizo/S
 miércoles
 mnemotecnia/S
+mnemotécnico/GS
 moa/S
 moabita/S
 moaré/S
@@ -37409,6 +37454,7 @@ multifamiliar/S
 multifloro/GS
 multiforme/S
 multilateral/S
+multilingüe/S
 multimedia/S
 multimillonario/GS
 multinacional/S
@@ -37421,6 +37467,7 @@ multiplicativo/GS
 multiplicidad/S
 multipolar/S
 multipropiedad/S
+multitarea
 multitud/S
 multitudinario/GS
 multiuso/S
@@ -37741,6 +37788,7 @@ narcisismo/S
 narcisista/S
 narciso/SO
 narcisísticamente
+narcolepsia/S
 narcosis
 narcotina/S
 narcotismo/S
@@ -37910,6 +37958,8 @@ nefritis
 nefrítico/GS
 nefrítico/S
 negable/kS
+negacionismo/S
+negacionista/S
 negación/gS
 negada/p
 negadamente
@@ -38224,6 +38274,7 @@ nodo/S
 nodriza/S
 nodular/S
 nodátil/S
+noes
 nogada/S
 nogal/S
 nogalina/S
@@ -38468,6 +38519,7 @@ nutra/S
 nutria/S
 nutricio/GS
 nutricional/S
+nutricionista/S
 nutrición/Sh
 nutrido/GS
 nutriente/S
@@ -38782,6 +38834,7 @@ odontalgia/S
 odontología/S
 odorífero/GS
 odre/S
+oes
 oeste
 ofendedor/GS
 ofender/REDÂñò
@@ -40081,7 +40134,7 @@ paraliticar/RED
 paralización/S
 paralizador/GS
 paralizante/S
-paralizar/REDÀÅñò
+paralizar/REDÀÅÄñò
 paralogizar/RED
 paraláctico/SG
 paralímpico/GS
@@ -40089,7 +40142,7 @@ paralítico/GS
 paramagnético/GS
 paramentar/RED
 paramento/nS
-parametrizar/RED
+parametrizar/REDA
 paramilitar/S
 paramétrico/GS
 parancero/S
@@ -40350,7 +40403,7 @@ pasante/S
 pasantía/S
 pasaportar/RED
 pasaporte/S
-pasar/pREDÀÃÄÅÇÍÌÏÙÛñòôøT
+pasar/pREDÀÃÄÅÆÇÍÌÏÙÛñòôøT
 pasarela/S
 pasatiempo/S
 pasavante/S
@@ -40629,7 +40682,7 @@ peajero/S
 peal/S
 peana/S
 peatonal/S
-peatonalizar/REDÀÄÌ
+peatonalizar/REDAÀÄÌ
 peatón/GS
 pebete/GS
 pebete/S
@@ -41328,6 +41381,7 @@ perrengue/S
 perrera/S
 perrero/GS
 perrezno/S
+perrillo/S
 perrito/GS
 perrito/S
 perro/NS
@@ -42719,6 +42773,7 @@ portaplumas
 portar/REDÁñòôø
 portaventanero/S
 portaviandas
+portaviones
 portavoz/S
 portazgar/RED
 portazgo/S
@@ -43053,6 +43108,7 @@ preeminencia/S
 preeminente/S
 preescolar/S
 preestablecido/SG
+preestreno/S
 preexcelso/GS
 preexistencia/S
 preexistente/S
@@ -43184,6 +43240,7 @@ presbiteriano/GS
 presbiterio/S
 presbítero/S
 presciencia/S
+presciente/S
 prescindible/Sk
 prescindir/RED
 prescribir/REDÂ
@@ -44342,6 +44399,7 @@ pícaro/GS
 pícea/S
 píceo/GS
 pícrico/S
+pífano/S
 píldora/S
 píloro/S
 pínula/S
@@ -44351,6 +44409,7 @@ pío/kGS
 pívot
 píxel/S
 pócima/S
+pódium
 pólenes
 pólipo/S
 póliza/S
@@ -45230,6 +45289,7 @@ recalcitrante/S
 recalcitrantemente
 recalcitrar/RED
 recalentar/IRDÀ
+recalificar/REDÀÁÂÄ
 recalvastro/GS
 recalzar/RED
 recamar/RED
@@ -45535,6 +45595,7 @@ redimible/kS
 redimir/REDÀÁÄñ
 redirigir/RED
 redistribuir/IRDÀT
+redistributivo/GS
 redituar/IRD
 redoblante/S
 redoblar/RED
@@ -45595,7 +45656,7 @@ reencontrar/IRDÀÂñò
 reencuadernar/RED
 reencuentro/S
 reenganchamiento/S
-reenganchar/RED
+reenganchar/REDÀ
 reengendrar/RED
 reensayar/RED
 reenviar/IRDÀ
@@ -46500,6 +46561,7 @@ resignadamente
 resignar/REDñôø
 resignatario/S
 resiliencia/S
+resiliente/S
 resina/S
 resinar/RED
 resinero/GS
@@ -49091,6 +49153,7 @@ sibilante/S
 sibilino/GS
 sic
 sicalipsis
+sicalíptico/GS
 sicario/S
 siciliano/GS
 siciliano/S
@@ -49549,6 +49612,7 @@ sobredorar/RED
 sobreedificar/RED
 sobreempeine/S
 sobreentender/IRD
+sobreesdrújulo/GS
 sobreexceder/RED
 sobreexcitar/RED
 sobreexplotación/S
@@ -49597,6 +49661,7 @@ sobrescribir/RE
 sobrescripto/GS
 sobrescrito/GS
 sobrescrito/S
+sobresdrújulo/GS
 sobreseer/REDÀ
 sobreseimiento/S
 sobresembrar/IRD
@@ -50267,7 +50332,7 @@ subyugar/REDÀÄT
 subíndice/S
 succenturiado/S
 succino/S
-succionar/REDÀÂ
+succionar/REDÀÂÄ
 succión/S
 suceder/REDÁÂÅ
 sucedido/S
@@ -51334,6 +51399,8 @@ telegráficamente
 telegráfico/GS
 teleindicador/S
 telele/S
+telemática
+telemático/GS
 telendo/GS
 telenovela/S
 teleobjetivo/S
@@ -52017,6 +52084,7 @@ tirajo/S
 tiralíneas
 tiramiento/pS
 tiramira/S
+tiramisú/S
 tiramollar/RED
 tiranicida/S
 tiranicidio/S
@@ -52662,6 +52730,7 @@ transatlántico/S
 transbordador/GS
 transbordador/S
 transbordar/RED
+transbordo/S
 transcendencia/S
 transcendental/S
 transcendentalismo/S
@@ -53284,6 +53353,7 @@ triquinosis
 triquitraque/S
 triquiñuela/S
 trirrectángulo/S
+trirreme/S
 tris/O
 trisagio/S
 trisar/RED
@@ -53735,7 +53805,7 @@ tímido/SG
 tímpano/S
 tío/GS
 típicamente
-típico/SG
+típico/SGa
 típula/S
 tísico/GS
 tít
@@ -53780,6 +53850,7 @@ ucase/S
 ucraniano/GS
 ucraniano/S
 ucranio/GS
+ues
 uf
 ufanamente
 ufanar/REDñò
@@ -56147,6 +56218,7 @@ zóster/S
 íd
 ídem
 ídolo/S
+íes
 ígneo/SG
 íleon/S
 ímpetu

es/es_BO.dic
@@ -1,4 +1,4 @@
-55763
+55835
 ABS
 ADSL
 Abapó
@@ -80,6 +80,7 @@ Burundi
 Bután
 Bélgica
 CD
+COVID
 Caicos
 Caledonia
 Camargo
@@ -3608,6 +3609,7 @@ alumbre/S
 aluminato/S
 aluminio/S
 aluminita/S
+aluminosis
 alumnado/S
 alumno/GS
 alunado/GS
@@ -4063,6 +4065,7 @@ anadear/RED
 anadina/S
 anadino/S
 anadiplosis
+anaeróbico/GS
 anafalla/S
 anafe/S
 anafilaxia/S
@@ -5309,6 +5312,8 @@ archivar/REDÀÙ
 archivero/GS
 archivista/S
 archivo/S
+archivología/S
+archivístico/GS
 arché
 arcilla/S
 arcillar/RED
@@ -6543,7 +6548,6 @@ aturullar/RED
 atusador/GS
 atusar/REDÂ
 atávico/SG
-atípico/GS
 atómico/sSG
 atónito/SG
 atópico/GS
@@ -6707,6 +6711,7 @@ autoritarismo/S
 autorización/hS
 autorizadamente
 autorizado/SG
+autorizador/GS
 autorizamiento/S
 autorizar/REDÀÁÂÄÅÆÚT
 autorregular/REDA
@@ -7718,6 +7723,7 @@ bastón/HNS
 basura/S
 basurear/RED
 basurero/GS
+basáltico/GS
 basílica/S
 bata/H
 bata/S
@@ -8060,6 +8066,7 @@ bibliología
 bibliometría
 biblioteca/S
 bibliotecario/GS
+biblioteconomía/S
 bicampeonato/S
 bicentenario/S
 bicha/S
@@ -8856,6 +8863,7 @@ bribar
 bribonada/S
 bribonear/RED
 bribón/GS
+bricbarca/S
 bricolaje/S
 brida/S
 briega/S
@@ -9221,9 +9229,11 @@ bóvido/SG
 bóxer/S
 búbalo/S
 búfalo/S
+búfer/S
 búho/S
 búlgaro/GS
 búlgaro/S
+búnker/S
 búsqueda/S
 ca
 cabal
@@ -12091,6 +12101,7 @@ cicatrizativo/GS
 cicerone/S
 ciceroniano/GS
 cicerón/S
+ciclable/S
 ciclamor/S
 ciclar/RED
 ciclismo/S
@@ -12914,6 +12925,7 @@ colmo/SG
 colocado/GS
 colocar/REDÀÁÃÅÆÇÌÍÛñòôøAÙ
 colocasia/S
+colocón/S
 colodión/S
 colodra/S
 colodrillo/S
@@ -13213,6 +13225,7 @@ compensar/REDÀÁÂÄÆñ
 compensativo/GS
 compensatorio/GS
 competencia/kS
+competencial/S
 competente/S
 competente/kS
 competentemente
@@ -13963,6 +13976,7 @@ construcción/pS
 constructiva/p
 constructivismo
 constructivo/pSG
+constructo/S
 constructor/SG
 construir/IRDÀÁÂÈ
 consubstancialidad/S
@@ -14083,6 +14097,7 @@ continuamente
 continuar/IRDÀÂÄ
 continuativo/GS
 continuidad/S
+continuista/S
 continuo
 continuo/S
 continuo/kGSh
@@ -14357,6 +14372,7 @@ convincente/S
 convincentemente
 convite/S
 conviviente/S
+convivio/S
 convivir/RED
 convocador/GS
 convocante/S
@@ -14579,6 +14595,7 @@ coronamiento/S
 coronar/REDÀÌñ
 coronaria/S
 coronario/GS
+coronavirus
 corondel/S
 coronel/S
 coronela/S
@@ -15004,6 +15021,7 @@ crieis
 crimen/S
 criminal/S
 criminalidad/S
+criminalizar/REDÀÁÄÌÙ
 criminalmente
 criminar/RED
 criminoso/GS
@@ -15172,8 +15190,9 @@ crédulo/kSG
 cría/pS
 crío/GS
 crítica/S
-críticamente
+críticamente/a
 crítico/GS
+crítico/GSa
 crómico/SG
 crónica/S
 crónico/SG
@@ -15678,6 +15697,7 @@ cámaro/S
 cámbaro/S
 cámbrica/n
 cámbrico/nGS
+cáncamo/S
 cáncer/S
 cándidamente
 cándido/GS
@@ -15807,6 +15827,7 @@ dadas
 dadivar
 dadivosidad/S
 dadivoso/GS
+dadle
 dadles
 dadme
 dado
@@ -16105,6 +16126,7 @@ deflación/S
 deflagración/S
 deflagrador/S
 deflagrador/SG
+deflagrante/cS
 deflagrar/RED
 deflegmar/RED
 deflexión/S
@@ -16279,6 +16301,7 @@ demolición/S
 demonche/S
 demonio/S
 demonización/S
+demonizar/RED
 demonología/S
 demonológico/GS
 demonomancia/S
@@ -16471,7 +16494,9 @@ derivativo/GS
 dermalgia/S
 dermatitis
 dermatoesqueleto/S
+dermatología/S
 dermatosis
+dermatólogo/GS
 dermesto/S
 dermis
 dermitis
@@ -17039,7 +17064,7 @@ deschavetar/RED
 deschuponar/RED
 descifrable/kS
 descifrador/GS
-descifrar/REDÀÂ
+descifrar/REDÀÂP
 descimbramiento/S
 descimbrar/RED
 descimentar/IRD
@@ -17174,7 +17199,7 @@ descuajar/RED
 descuajaringar/RED
 descuajeringar/RED
 descuartizamiento/S
-descuartizar/REDÀÁÄ
+descuartizar/REDÀÁÄÌ
 descubiertamente
 descubierto/GS
 descubridero/S
@@ -17384,7 +17409,7 @@ desenfreno/S
 desenfundar/REDÀ
 desenfurecer/IRD
 desenfurruñar/RED
-desenganchar/REDñ
+desenganchar/REDÀñ
 desengarrafar
 desengarzar/RED
 desengastar/RED
@@ -18789,6 +18814,7 @@ dinamitar/REDÀ
 dinamitazo/S
 dinamitero/GS
 dinamización/S
+dinamizador/GS
 dinamizar/REDÀ
 dinamo/S
 dinamómetro/S
@@ -21317,6 +21343,7 @@ endurecer/IRDÀÄÅò
 endurecimiento/S
 endémico/SG
 ene
+ene/S
 enea/S
 eneal/S
 eneasílabo/GS
@@ -21455,7 +21482,7 @@ engallar/RED
 engalle/S
 enganchador/GS
 enganchamiento/S
-enganchar/REDÄñô
+enganchar/REDÀÄñô
 enganche/S
 engandujar/RED
 engarabatar/RED
@@ -23081,6 +23108,7 @@ escénico/GS
 escéptico/GS
 escúter/S
 esdrujulizar/RED
+esdrújulo/GS
 ese/G
 ese/GS
 ese/S
@@ -23499,6 +23527,7 @@ esposa/S
 esposar/REDÀÂ
 esposo/GS
 espray
+esprint
 espráis
 espuela/S
 espuerta/S
@@ -24107,6 +24136,7 @@ estulticia/S
 estulto/SG
 estuosidad/S
 estuoso/SG
+estupa/S
 estupefacción/S
 estupefaciente/S
 estupefactivo/GS
@@ -24802,6 +24832,7 @@ exótico/SG
 eyaculación/S
 eyacular/REDÁ
 ezquerdear/RED
+eñe/S
 eólico/GS
 eón/S
 fa
@@ -24976,6 +25007,7 @@ familiarmente
 famosamente
 famoso/GS
 famélico/GS
+fan/S
 fanal/S
 fanatismo/S
 fanatizador/GS
@@ -26443,6 +26475,7 @@ fálico/GS
 fárfara/S
 fármaco/S
 fárrago/S
+fático/SG
 fécula/S
 félido/GS
 félido/S
@@ -26801,6 +26834,7 @@ garra/S
 garrafa/S
 garrafal/S
 garrafiñar/RED
+garrafón/S
 garramar/RED
 garrancha/S
 garrancho/SH
@@ -27108,7 +27142,7 @@ gesticulantemente
 gesticular/RED
 gesticular/S
 gesticuloso/GS
-gestionar/REDÀÂ
+gestionar/REDÁÀÂ
 gestión/fSkd
 gesto/S
 gestor/GS
@@ -27424,6 +27458,7 @@ graduando/GS
 graduar/IRDÁÂñT
 grafeno/S
 graficar/RED
+grafitero/GS
 grafiti/S
 grafito/S
 grafo/S
@@ -29617,6 +29652,7 @@ húmedo/GS
 húmero/S
 húngaro/S
 húngaro/SG
+i
 ib
 iba
 iban
@@ -30264,6 +30300,7 @@ indigesto/SG
 indignación/S
 indignadísimo/GS
 indignamente
+indignante/S
 indignar/REDÁÂñ
 indignidad/S
 indilgar/RED
@@ -30439,6 +30476,7 @@ infirmar/RED
 inflacionario/GS
 inflacionista/S
 inflación/S
+inflador/GS
 inflamable/kS
 inflamación/S
 inflamador/GS
@@ -31077,7 +31115,7 @@ invenciblemente
 invención/S
 invenir
 inventar/REDÀÁÂÃÄÅÆÍ
-inventariar/IRDÀ
+inventariar/IRDÀT
 inventario/S
 inventiva/S
 inventivo/SG
@@ -31943,6 +31981,7 @@ juégalo
 juégalos
 jábega/S
 jácara/S
+jáquer/S
 jáquima/S
 jíbaro/GS
 jíbaro/SG
@@ -31970,6 +32009,7 @@ keniano/GS
 keniata/S
 kermes
 ketchup/S
+kg
 kiliárea/S
 kilo/S
 kilociclo/S
@@ -32484,6 +32524,7 @@ lazdrar
 lazo/Sa
 lazrar
 lazulita/S
+laísmo/S
 lañar/RED
 laúd/S
 ldo/G
@@ -32779,6 +32820,7 @@ lezna/S
 lezne/GS
 leída/S
 leído/GS
+leísmo/S
 leña/S
 leñador/GS
 leño/S
@@ -33470,6 +33512,7 @@ lozanear/RED
 lozanecer/IRD
 lozano/GS
 lozanía/S
+loísmo/S
 ltdo/G
 lubigante/S
 lubina/S
@@ -35125,6 +35168,7 @@ mecánicamente
 mecánico/GS
 mecánico/SG
 medalla/SU
+medallista/S
 medanoso/GS
 medellinense/S
 media/S
@@ -35836,7 +35880,7 @@ miera/S
 mierda/S
 mierla/S
 mierra/S
-mies
+mies/S
 miga/Sa
 migaja/S
 migajada/S
@@ -36152,6 +36196,7 @@ mizo/GS
 mizo/S
 miércoles
 mnemotecnia/S
+mnemotécnico/GS
 moa/S
 moabita/S
 moaré/S
@@ -37033,6 +37078,7 @@ multifamiliar/S
 multifloro/GS
 multiforme/S
 multilateral/S
+multilingüe/S
 multimedia/S
 multimillonario/GS
 multinacional/S
@@ -37045,6 +37091,7 @@ multiplicativo/GS
 multiplicidad/S
 multipolar/S
 multipropiedad/S
+multitarea
 multitud/S
 multitudinario/GS
 multiuso/S
@@ -37365,6 +37412,7 @@ narcisismo/S
 narcisista/S
 narciso/SO
 narcisísticamente
+narcolepsia/S
 narcosis
 narcotina/S
 narcotismo/S
@@ -37534,6 +37582,8 @@ nefritis
 nefrítico/GS
 nefrítico/S
 negable/kS
+negacionismo/S
+negacionista/S
 negación/gS
 negada/p
 negadamente
@@ -37846,6 +37896,7 @@ nodo/S
 nodriza/S
 nodular/S
 nodátil/S
+noes
 nogada/S
 nogal/S
 nogalina/S
@@ -38088,6 +38139,7 @@ nutra/S
 nutria/S
 nutricio/GS
 nutricional/S
+nutricionista/S
 nutrición/Sh
 nutrido/GS
 nutriente/S
@@ -38400,6 +38452,7 @@ odontalgia/S
 odontología/S
 odorífero/GS
 odre/S
+oes
 oeste
 ofendedor/GS
 ofender/REDÂñò
@@ -39675,7 +39728,7 @@ paraliticar/RED
 paralización/S
 paralizador/GS
 paralizante/S
-paralizar/REDÀÅñò
+paralizar/REDÀÅÄñò
 paralogizar/RED
 paraláctico/SG
 paralímpico/GS
@@ -39683,7 +39736,7 @@ paralítico/GS
 paramagnético/GS
 paramentar/RED
 paramento/nS
-parametrizar/RED
+parametrizar/REDA
 paramilitar/S
 paramétrico/GS
 parancero/S
@@ -39945,7 +39998,7 @@ pasante/S
 pasantía/S
 pasaportar/RED
 pasaporte/S
-pasar/pREDÀÃÄÅÇÍÌÏÙÛñòôøT
+pasar/pREDÀÃÄÅÆÇÍÌÏÙÛñòôøT
 pasarela/S
 pasatiempo/S
 pasavante/S
@@ -40222,7 +40275,7 @@ peajero/S
 peal/S
 peana/S
 peatonal/S
-peatonalizar/REDÀÄÌ
+peatonalizar/REDAÀÄÌ
 peatón/GS
 pebete/S
 pebetero/S
@@ -40916,6 +40969,7 @@ perrengue/S
 perrera/S
 perrero/GS
 perrezno/S
+perrillo/S
 perrito/GS
 perrito/S
 perro/NS
@@ -42294,6 +42348,7 @@ portaplumas
 portar/REDÁñòôø
 portaventanero/S
 portaviandas
+portaviones
 portavoz/S
 portazgar/RED
 portazgo/S
@@ -42626,6 +42681,7 @@ preeminencia/S
 preeminente/S
 preescolar/S
 preestablecido/SG
+preestreno/S
 preexcelso/GS
 preexistencia/S
 preexistente/S
@@ -42757,6 +42813,7 @@ presbiteriano/GS
 presbiterio/S
 presbítero/S
 presciencia/S
+presciente/S
 prescindible/Sk
 prescindir/RED
 prescribir/REDÂ
@@ -43906,6 +43963,7 @@ pícaro/GS
 pícea/S
 píceo/GS
 pícrico/S
+pífano/S
 píldora/S
 píloro/S
 pínula/S
@@ -43915,6 +43973,7 @@ pío/kGS
 pívot
 píxel/S
 pócima/S
+pódium
 pólenes
 pólipo/S
 póliza/S
@@ -44793,6 +44852,7 @@ recalcitrante/S
 recalcitrantemente
 recalcitrar/RED
 recalentar/IRDÀ
+recalificar/REDÀÁÂÄ
 recalvastro/GS
 recalzar/RED
 recamar/RED
@@ -45096,6 +45156,7 @@ redimible/kS
 redimir/REDÀÁÄñ
 redirigir/RED
 redistribuir/IRDÀT
+redistributivo/GS
 redituar/IRD
 redoblante/S
 redoblar/RED
@@ -45156,7 +45217,7 @@ reencontrar/IRDÀÂñò
 reencuadernar/RED
 reencuentro/S
 reenganchamiento/S
-reenganchar/RED
+reenganchar/REDÀ
 reengendrar/RED
 reensayar/RED
 reenviar/IRDÀ
@@ -46056,6 +46117,7 @@ resignadamente
 resignar/REDñôø
 resignatario/S
 resiliencia/S
+resiliente/S
 resina/S
 resinar/RED
 resinero/GS
@@ -48624,6 +48686,7 @@ sibilante/S
 sibilino/GS
 sic
 sicalipsis
+sicalíptico/GS
 sicario/S
 siciliano/GS
 siciliano/S
@@ -49081,6 +49144,7 @@ sobredorar/RED
 sobreedificar/RED
 sobreempeine/S
 sobreentender/IRD
+sobreesdrújulo/GS
 sobreexceder/RED
 sobreexcitar/RED
 sobreexplotación/S
@@ -49125,6 +49189,7 @@ sobresanar/RED
 sobrescribir/RE
 sobrescrito/GS
 sobrescrito/S
+sobresdrújulo/GS
 sobreseer/REDÀ
 sobreseimiento/S
 sobresembrar/IRD
@@ -49791,7 +49856,7 @@ subyugar/REDÀÄT
 subíndice/S
 succenturiado/S
 succino/S
-succionar/REDÀÂ
+succionar/REDÀÂÄ
 succión/S
 suceder/REDÁÂÅ
 sucedido/S
@@ -50853,6 +50918,8 @@ telegráficamente
 telegráfico/GS
 teleindicador/S
 telele/S
+telemática
+telemático/GS
 telendo/GS
 telenovela/S
 teleobjetivo/S
@@ -51532,6 +51599,7 @@ tirajo/S
 tiralíneas
 tiramiento/pS
 tiramira/S
+tiramisú/S
 tiramollar/RED
 tiranicida/S
 tiranicidio/S
@@ -52173,6 +52241,7 @@ transatlántico/S
 transbordador/GS
 transbordador/S
 transbordar/RED
+transbordo/S
 transcendencia/S
 transcendental/S
 transcendentalismo/S
@@ -52791,6 +52860,7 @@ triquinosis
 triquitraque/S
 triquiñuela/S
 trirrectángulo/S
+trirreme/S
 tris/O
 trisagio/S
 trisar/RED
@@ -53239,7 +53309,7 @@ tímido/SG
 tímpano/S
 tío/GS
 típicamente
-típico/SG
+típico/SGa
 típula/S
 tísico/GS
 tít
@@ -53283,6 +53353,7 @@ ucase/S
 ucraniano/GS
 ucraniano/S
 ucranio/GS
+ues
 uf
 ufanamente
 ufanar/REDñò
@@ -55607,6 +55678,7 @@ zóster/S
 íd
 ídem
 ídolo/S
+íes
 ígneo/SG
 íleon/S
 ímpetu

commit 3978d026b57097ede0be8ac6e4ffd1164ada7474

Fix path of logo for Spanish dictionary
Regression from c1ddcf602ba97f8e99383f34e8027074d8193f61
"Updating RLA-ES Spanish spellcheker to v2.8."

There's no "images" subdir in "dictionaries/es"

Change-Id: Ia18110dbe04feec6553fb80501450546a0ac79a2
Reviewed-on: https://gerrit.libreoffice.org/c/dictionaries/+/158169
Reviewed-by: Adolfo Jayme Barrientos <fitojb@ubuntu.com>
Tested-by: Adolfo Jayme Barrientos <fitojb@ubuntu.com>

Dictionary_es.mk
@@ -9,10 +9,6 @@
 
 $(eval $(call gb_Dictionary_Dictionary,dict-es,dictionaries/es))
 
-$(eval $(call gb_Dictionary_add_files,dict-es,images,\
-    dictionaries/es/RLA-ES.png \
-))
-
 $(eval $(call gb_Dictionary_add_root_files,dict-es,\
 	dictionaries/es/es_AR.aff \
 	dictionaries/es/es_AR.dic \
@@ -66,6 +62,7 @@ $(eval $(call gb_Dictionary_add_root_files,dict-es,\
 	dictionaries/es/README_hyph_es.txt \
 	dictionaries/es/README_th_es.txt \
 	dictionaries/es/LICENSE.md \
+	dictionaries/es/RLA-ES.png \
 ))
 
 $(eval $(call gb_Dictionary_add_thesauri,dict-es,\