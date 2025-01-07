e7b61087d7d42eba51425eae74643ea5623c574a
Shantanu Oak <shantanu.oak@gmail.com>
2025-01-07 03:54:22 +0000

    Add Marathi & Bengali
    Change-Id: I18bfa668abba41b9ac494584dbca7a40920e2e6b
    Reviewed-on: https://gerrit.libreoffice.org/c/dictionaries/+/179851
    Reviewed-by: Ilmari Lauhakangas <ilmari.lauhakangas@libreoffice.org>
    Tested-by: Ilmari Lauhakangas <ilmari.lauhakangas@libreoffice.org>

--- Dictionary_mr.mk
+++ Dictionary_mr.mk
@@ -7,14 +7,14 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
 
-$(eval $(call gb_Dictionary_Dictionary,dict-bn,dictionaries/bn_BD))
+$(eval $(call gb_Dictionary_Dictionary,dict-mr,dictionaries/mr_IN))
 
-$(eval $(call gb_Dictionary_add_root_files,dict-bn,\
-        dictionaries/bn_BD/bn_BD.aff \
-        dictionaries/bn_BD/bn_BD.dic \
-        dictionaries/bn_BD/bn_IN.aff \
-        dictionaries/bn_BD/bn_IN.dic \
-        dictionaries/bn_BD/COPYING \
+$(eval $(call gb_Dictionary_add_root_files,dict-mr,\
+        dictionaries/mr_IN/mr_IN.aff \
+        dictionaries/mr_IN/mr_IN.dic \
+        dictionaries/mr_IN/COPYING \
+
 ))
 
 # vim: set noet sw=4 ts=4:
+
